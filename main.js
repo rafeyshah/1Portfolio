ab39f4e9a2333ebb84a1a98610e3890de0d4c7da
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2021-01-11 11:37:46 +0500

    7

--- main.js
+++ main.js
@@ -71,3 +71,13 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+const sharp = require('sharp');
+const fs = require('fs');
+const directory = './images';
+
+fs.readdirSync(directory).forEach(file => {
+  sharp(`${directory}/${file}`)
+    .resize(200, 100) // width, height
+    .toFile(`${directory}/${file}-small.jpg`);
+  });
