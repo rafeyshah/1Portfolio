16798ebb13b57f5fcd444b0947b6692c696b9c15
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2021-01-13 12:36:11 +0500

    12

--- main.js
+++ main.js
@@ -54,6 +54,8 @@ if ('serviceWorker' in navigator) {
     });
   }
 
+  navigator.serviceWorker.getRegistrations().then(function(registrations) { for(let registration of registrations) { registration.unregister() } })
+
   var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
